25d0c64c43b5f420fc7c86fcc6de82878fc5ac1c
jamesborn89 <jamesborn89@gmail.com>
2020-08-19 15:31:04 +0400

    added media

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(function(){
 
+  $('.header__menu-burger, .header__menu-list').on('click', function(){
+    $('.header__menu-burger').toggleClass('active');
+  });
+
+  $('.header__menu-burger, .header__menu-list').click( function(){
+      $('.header__menu-list').slideToggle();
+    });
 
   });
